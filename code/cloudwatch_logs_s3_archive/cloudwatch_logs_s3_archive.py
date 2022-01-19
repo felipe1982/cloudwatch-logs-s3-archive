14c172e6fe12e43ae67a2e8bb7f92c741aaae41c
Felipe Alvarez <felipe.alvarez@versent.com.au>
2022-01-19 16:44:05 +1000

    test: move import to each test function

--- code/cloudwatch_logs_s3_archive/cloudwatch_logs_s3_archive.py
+++ code/cloudwatch_logs_s3_archive/cloudwatch_logs_s3_archive.py
@@ -8,14 +8,10 @@ from botocore.exceptions import ClientError
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
-# logs = boto3.client("logs")
-# ssm = boto3.client("ssm")
 
 
 class CloudWatchLogsS3Archive:
     botocore_config = Config(retries={"max_attempts": 10, "mode": "adaptive"})
-    logs = boto3.client("logs", config=botocore_config)
-    ssm = boto3.client("ssm", config=botocore_config)
 
     def __init__(self, s3_bucket, account_id) -> None:
         self.s3_bucket = s3_bucket
@@ -23,6 +19,8 @@ class CloudWatchLogsS3Archive:
         self.extra_args = {}
         self.log_groups = []
         self.log_groups_to_export = []
+        self.logs = boto3.client("logs", config=self.botocore_config)
+        self.ssm = boto3.client("ssm", config=self.botocore_config)
 
     def check_valid_inputs(self):
         """Check that required inputs are present and valid"""
@@ -30,18 +28,19 @@ class CloudWatchLogsS3Archive:
             logging.error("Account Id must be valid 12-digit AWS account id")
             raise ValueError("Account Id must be valid 12-digit AWS account id")
 
-    def collect_log_groups_list(self):
+    def collect_log_groups(self):
         """Capture the names of all of the CloudWatch Log Groups"""
-        paginator = self.__class__.logs.get_paginator("describe_log_groups")
+        paginator = self.logs.get_paginator("describe_log_groups")
         page_it = paginator.paginate()
         for p in page_it:
-            return (lg["logGroupName"] for lg in p["logGroups"])  # type: ignore
+            for lg in p["logGroups"]:
+                yield lg["logGroupName"]
 
     def get_last_export_time(self, Name) -> str:
         """Get time of the last export from SSM Parameter Store"""
         try:
-            return self.__class__.ssm.get_parameter(Name=Name)["Parameter"]["Value"]
-        except (self.__class__.ssm.exceptions.ParameterNotFound, ClientError) as exc:
+            return self.ssm.get_parameter(Name=Name)["Parameter"]["Value"]
+        except (self.ssm.exceptions.ParameterNotFound, ClientError) as exc:
             logger.warning(*exc.args)
             if exc.response["Error"]["Code"] == "ParameterNotFound":  # type: ignore
                 return "0"
@@ -50,49 +49,49 @@ class CloudWatchLogsS3Archive:
 
     def set_export_time(self):
         """Set current export time"""
-
         return round(time() * 1000)
 
     def put_export_time(self, put_time, Name):
         """Put current export time to SSM Parameter Store"""
-        self.__class__.ssm.put_parameter(Name=Name, Value=str(put_time))
+        self.ssm.put_parameter(Name=Name, Value=str(put_time), Overwrite=True)
 
     def create_export_tasks(
         self, log_group_name, fromTime, toTime, s3_bucket, account_id
     ):
         """Create new CloudWatchLogs Export Tasks"""
-        # try:
-        response = self.__class__.logs.create_export_task(
-            logGroupName=log_group_name,
-            fromTime=int(fromTime),
-            to=toTime,
-            destination=s3_bucket,
-            destinationPrefix="{}/{}".format(account_id, log_group_name.strip("/")),
-        )
-        logger.info("✔   Task created: %s" % response["taskId"])
-        # except logs.exceptions.LimitExceededException:
-        #     """The Boto3 standard retry mode will catch throttling errors and
-        #     exceptions, and will back off and retry them for you."""
-        #     logger.warning(
-        #         "⚠   Too many concurrently running export tasks "
-        #         "(LimitExceededException); backing off and retrying..."
-        #     )
-        #     return False
-        # except Exception as e:
-        #     logger.exception(
-        #         "✖   Error exporting %s: %s",
-        #         log_group_name,
-        #         getattr(e, "message", repr(e)),
-        #     )
-
-        # ssm_response = self.__class__.ssm.put_parameter(
-        #     Name=ssm_parameter_name,
-        #     Type="String",
-        #     Value=str(export_to_time),
-        #     Overwrite=True,
-        # )
+        try:
+            response = self.logs.create_export_task(
+                logGroupName=log_group_name,
+                fromTime=int(fromTime),
+                to=toTime,
+                destination=s3_bucket,
+                destinationPrefix="{}/{}".format(account_id, log_group_name.strip("/")),
+            )
+            logger.info("✔   Task created: %s" % response["taskId"])
+        except self.logs.exceptions.LimitExceededException:
+            """The Boto3 standard retry mode will catch throttling errors and
+            exceptions, and will back off and retry them for you."""
+            logger.warning(
+                "⚠   Too many concurrently running export tasks "
+                "(LimitExceededException); backing off and retrying..."
+            )
+            # return False
+        except Exception as e:
+            logger.exception(
+                "✖   Error exporting %s: %s",
+                log_group_name,
+                getattr(e, "message", repr(e)),
+            )
 
 
 def lambda_handler(event, context):
-    os.environ["S3_BUCKET"] = "mybucket2"
-    os.environ["ACCOUNT_ID"] = "123412341234"
+    s3_bucket = os.environ["S3_BUCKET"]
+    account_id = os.environ["ACCOUNT_ID"]
+    c = CloudWatchLogsS3Archive(s3_bucket, account_id)
+    c.check_valid_inputs()
+    log_groups = c.collect_log_groups()
+    for log_group_name in log_groups:
+        fromTime = c.get_last_export_time(log_group_name)
+        toTime = c.set_export_time()
+        c.create_export_tasks(log_group_name, fromTime, toTime, s3_bucket, account_id)
+        c.put_export_time(log_group_name, toTime)
--- code/cloudwatch_logs_s3_archive/test_mock_cloudwatch_logs_s3_archive.py
+++ code/cloudwatch_logs_s3_archive/test_mock_cloudwatch_logs_s3_archive.py
@@ -1,5 +1,4 @@
 """Test Cloud Watch Logs S3 Archive Python Lambda Function"""
-
 import logging
 import os
 from unittest import mock
@@ -8,20 +7,12 @@ import boto3
 import moto
 import pytest
 
-from cloudwatch_logs_s3_archive import CloudWatchLogsS3Archive
-
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
 
 
 @pytest.fixture
-def f_env_vars():
-    os.environ["S3_BUCKET"] = "mybucket2"
-    os.environ["ACCOUNT_ID"] = "123412341234"
-
-
-@pytest.fixture
-def f_aws_credentials():
+def f_aws_credentials(autouse=True):
     """Mocked AWS Credentials for moto.
 
     This is a "side effect" function and None is returned because we are
@@ -34,8 +25,10 @@ def f_aws_credentials():
     os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
 
 
-def test_throw_exception_with_invalid_or_insufficient_inputs():
-    with pytest.raises((NameError, ValueError, TypeError)):
+def test_throw_TypeError_exception_with_invalid_or_insufficient_inputs():
+    from cloudwatch_logs_s3_archive import CloudWatchLogsS3Archive
+
+    with pytest.raises((TypeError)):
         c = CloudWatchLogsS3Archive()  # type: ignore
         c.check_valid_inputs()
 
@@ -43,13 +36,15 @@ def test_throw_exception_with_invalid_or_insufficient_inputs():
 @moto.mock_logs
 def test_get_a_generator_of_logs_groups():
     # ARRANGE
+    from cloudwatch_logs_s3_archive import CloudWatchLogsS3Archive
+
     logs = boto3.client("logs")
     logs.create_log_group(logGroupName="first")
     logs.create_log_group(logGroupName="second")
     logs.create_log_group(logGroupName="third")
     c = CloudWatchLogsS3Archive("bucket", 123412341234)
     # ACT
-    log_groups = c.collect_log_groups_list()
+    log_groups = c.collect_log_groups()
     # ASSERT
     assert next(log_groups) == "first"  # type: ignore
     assert next(log_groups) == "second"  # type: ignore
@@ -59,6 +54,8 @@ def test_get_a_generator_of_logs_groups():
 @moto.mock_ssm
 def test_get_last_export_time_from_ssm_parameter():
     from time import time
+
+    from cloudwatch_logs_s3_archive import CloudWatchLogsS3Archive
 
     expected = str(round(time() * 1000))
     ssm = boto3.client("ssm")
@@ -70,12 +67,16 @@ def test_get_last_export_time_from_ssm_parameter():
 
 @moto.mock_ssm
 def test_set_start_time_zero_when_parameter_does_not_exist():
+    from cloudwatch_logs_s3_archive import CloudWatchLogsS3Archive
+
     c = CloudWatchLogsS3Archive("bucket", "123412341234")
     last_export_time = c.get_last_export_time(Name="/doesnotexist")
     assert last_export_time == "0"
 
 
 def test_set_export_time():
+    from cloudwatch_logs_s3_archive import CloudWatchLogsS3Archive
+
     c = CloudWatchLogsS3Archive("bucket", "123412341234")
     from time import time
 
@@ -84,6 +85,8 @@ def test_set_export_time():
 
 @moto.mock_ssm
 def test_put_export_time():
+    from cloudwatch_logs_s3_archive import CloudWatchLogsS3Archive
+
     c = CloudWatchLogsS3Archive("bucket", "123412341234")
     put_time = 1642568042037
     c.put_export_time(
@@ -96,59 +99,52 @@ def test_put_export_time():
 @moto.mock_logs
 @moto.mock_ssm
 def test_create_export_tasks():
+    from cloudwatch_logs_s3_archive import CloudWatchLogsS3Archive
+
     c = CloudWatchLogsS3Archive("bucket", "123412341234")
     # ssm = boto3.client("ssm")
     logs = boto3.client("logs")
     logs.create_log_group(logGroupName="/log-exporter-last-export/first")
     logs.create_log_group(logGroupName="/log-exporter-last-export/second")
     logs.create_log_group(logGroupName="/log-exporter-last-export/third")
+    log_group_name = "/log-exporter-last-export/first"
+    s3_bucket = "s3_bucket"
+    account_id = 123412341234
     toTime = c.put_export_time(
         1642568042037,
         Name="/log-exporter-last-export/first",
     )
     fromTime = c.get_last_export_time(Name="/log-exporter-last-export/first")
-
-    with mock.patch(
-        target="cloudwatch_logs_s3_archive.CloudWatchLogsS3Archive.logs",
-        return_value={"taskId": "I am mocked via mock.patch"},
-        autospec=True,
-    ):
-        c.create_export_tasks(
-            "/log-exporter-last-export/first", fromTime, toTime, "s3_bucket", 123412341234
-        )
-#         target="cloudwatch_logs_s3_archive.CloudWatchLogsS3Archive.create_export_tasks",
+    c.logs.create_export_task = mock.Mock(
+        return_value={"taskId": "I am mocked via mock.Mock"}
+    )
+    c.create_export_tasks(
+        "/log-exporter-last-export/first", fromTime, toTime, "s3_bucket", 123412341234
+    )
+    assert c.logs.create_export_task.called
+    c.logs.create_export_task.assert_called
+    c.logs.create_export_task.assert_called_with(
+        logGroupName=log_group_name,
+        fromTime=int(fromTime),
+        to=toTime,
+        destination=s3_bucket,
+        destinationPrefix="{}/{}".format(account_id, log_group_name.strip("/")),
+    )
 
 
 def test_try_catch_LimitExceededException():
     ...
 
 
-##############################################################################
+@moto.mock_logs
+@moto.mock_s3
+@moto.mock_ssm
+def test_lambda_handler_function():
+    from cloudwatch_logs_s3_archive import lambda_handler
 
-# TODO
-# assert called_with(destination='mybucket2',
-#                     destinationPrefix='123412341234/mock_my_log_group_name',
-#                     fromTime=0,
-#                     logGroupName='mock_my_log_group_name',
-#                     to=1642488495636)
+    event = context = None
+    os.environ["S3_BUCKET"] = "mybucket"
+    os.environ["ACCOUNT_ID"] = "123412341234"
 
-# TODO assert ssm parameter for "fromTime" is present, and shows up in the
-# code, in the call to "create_export_task"
+    lambda_handler(event, context)
 
-# TODO
-# expected_calls = [call(...), call("pos1", "pos2", ID=1234, Name="felipe")]
-# investiage self.assert_has_calls(expected_calls)
-
-
-# # THIS IS FAILING : TODO: create_export_task.assert_any_call()
-# #
-# # MagicMock Methods
-# #
-# LOGS = mock.MagicMock()
-# # LOGS.assert_called()  (self.call_count == 0; raise AssertionError)
-# # LOGS.assert_has_calls()  # Required parameters
-# # LOGS.assert_any_call()  # raise AssertionError
-# LOGS.call_count  # int
-# LOGS.call_args  # Any
-# LOGS.call_args_list  # _Call_List
-# # LOGS.called  # bool (self.call_count == 0; raise AssertionError)
